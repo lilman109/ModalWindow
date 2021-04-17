3a0eef7662b43274bc7ead98ed0e8bd1d25346a1
lilman109 <akira.yoshidasjsu@gmail.com>
2021-04-17 12:13:05 +0900

    deleted starter directory and moved files to ModalWindow directly

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overylay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i, btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i]);
+}
