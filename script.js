2f08cbaa0fc942bedfb2c7e99677153cf9024c6f
lilman109 <akira.yoshidasjsu@gmail.com>
2021-04-17 13:51:31 +0900

    open modal, clos modal, key press implemented

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overylay');
+const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i, btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i]);
-}
+const onButtonClicked = (txt) => {
+	console.log(`${txt} button clicked!`);
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+	if (modal.classList.contains('hidden')) return;
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+const onEnterKeyPressed = (event) => {
+	if (event.key === 'Escape') {
+		closeModal();
+	}
+};
+
+btnsOpenModal.forEach((btn) => {
+	btn.addEventListener('click', () => onButtonClicked(btn.textContent));
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', (key) => onEnterKeyPressed(key));
